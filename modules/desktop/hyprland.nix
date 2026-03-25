c4d948c4a0d518914197f0a9c0d6b13ca30c1433
scottk817 <scott.keenan07@protonmail.com>
2026-03-25 04:12:05 -0400

    lets try a new theme and style

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -11,43 +11,32 @@ let
   red = "#bf616a";
   teal = "#8fbcbb";
   wallpaperDir = ../../assets/wallpapers;
-  sddmBackground = pkgs.runCommandLocal "sddm-background" {
-    nativeBuildInputs = [ pkgs.imagemagick ];
-  } ''
-    mkdir -p $out
-    magick -size 1920x1080 xc:'#232733' $out/background.png
-  '';
   sddmThemeConfig = pkgs.writeText "theme.conf" ''
     [General]
-    background=${sddmBackground}/background.png
-    backgroundMode=fill
-    backgroundFill=#232733
-    basicTextColor=#eceff4
-    passwordCharacter=*
-    passwordMask=true
-    passwordFontSize=16
-    passwordInputWidth=0.24
-    passwordInputBackground=#2b3040
-    passwordInputRadius=0
-    passwordCursorColor=#d580ff
-    passwordTextColor=#eceff4
-    usersFontSize=18
-    sessionsFontSize=12
-    font=JetBrainsMono Nerd Font
-    showSessionsByDefault=true
-    showUsersByDefault=true
-    showUserRealNameByDefault=false
-    hideCursor=false
+    primaryShade=#232733
+    primaryLight=#2b3040
+    primaryDark=#1f2330
+    primaryHue1=#d580ff
+    primaryHue2=#8fbcbb
+    primaryHue3=#d8dee9
+    accentShade=#2b3040
+    accentLight=#3b4252
+    accentHue1=#d580ff
+    accentHue2=#8fbcbb
+    accentHue3=#eceff4
+    normalText=#eceff4
+    successText=#8fbcbb
+    failureText=#bf616a
+    warningText=#d580ff
+    rebootColor=#8fbcbb
+    powerColor=#bf616a
   '';
-  sddmThemePackage = pkgs.where-is-my-sddm-theme.overrideAttrs (old: {
-    postInstall = (old.postInstall or "") + ''
-      cp ${sddmThemeConfig} $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf
-    '';
-    meta = old.meta or { };
-    passthru = old.passthru or { };
-    name = "where-is-my-sddm-theme-custom";
-    pname = "where-is-my-sddm-theme-custom";
-  });
+  sddmThemePackage = pkgs.runCommand "maya-custom-sddm-theme" {} ''
+    mkdir -p $out/share/sddm/themes
+    cp -R ${pkgs.kdePackages.sddm}/share/sddm/themes/maya $out/share/sddm/themes/maya-custom
+    chmod -R u+w $out/share/sddm/themes/maya-custom
+    cp ${sddmThemeConfig} $out/share/sddm/themes/maya-custom/theme.conf
+  '';
 
   wallpaperCtl = pkgs.writeShellScriptBin "wallpaperctl" ''
     set -euo pipefail
@@ -977,7 +966,7 @@ in
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = false;
-    theme = "where_is_my_sddm_theme";
+    theme = "maya-custom";
     extraPackages = [
       pkgs.qt6.qt5compat
       sddmThemePackage
@@ -987,7 +976,7 @@ in
         DisplayServer = "x11";
       };
       Theme = {
-        Current = "where_is_my_sddm_theme";
+        Current = "maya-custom";
         CursorTheme = "Adwaita";
         CursorSize = 24;
       };
